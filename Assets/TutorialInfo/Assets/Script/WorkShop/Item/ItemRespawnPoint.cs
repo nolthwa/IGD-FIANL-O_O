fbf91a9ce0b079185915a1c4f19618b5155b500f
nolthwa <nolthawat692547@gmail.com>
2025-11-24 20:18:36 +0700

    up_2
    respawn_onBox

--- Assets/TutorialInfo/Assets/Script/WorkShop/Item/ItemRespawnPoint.cs
+++ Assets/TutorialInfo/Assets/Script/WorkShop/Item/ItemRespawnPoint.cs
@@ -1,12 +1,15 @@
-﻿using  UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
 public class ItemRespawnPoint : Item
 {
-    
-    public GameObject[] itemPrefabs;   
+    public GameObject[] itemPrefabs;
 
-    public float respawnTime = 5f;   
+    public float respawnTime = 5f;
+
+    [Header("Respawn Area")]
+    public Vector3 areaSize = new Vector3(5f, 0f, 5f);  // ขนาดของพื้นที่สุ่ม
+
     private GameObject currentItem;
 
     void Start()
@@ -18,14 +21,23 @@ public class ItemRespawnPoint : Item
     {
         if (currentItem != null) return;
 
-       
+        // สุ่มตำแหน่งภายในกรอบ
+        Vector3 randomOffset = new Vector3(
+            Random.Range(-areaSize.x / 2, areaSize.x / 2),
+            Random.Range(-areaSize.y / 2, areaSize.y / 2),
+            Random.Range(-areaSize.z / 2, areaSize.z / 2)
+        );
+
+        Vector3 spawnPos = transform.position + randomOffset;
+
+        // เลือก prefab แบบสุ่ม
         int randomIndex = Random.Range(0, itemPrefabs.Length);
         GameObject prefabToSpawn = itemPrefabs[randomIndex];
 
+        // สร้างไอเท็ม
+        currentItem = Instantiate(prefabToSpawn, spawnPos, transform.rotation);
 
-        currentItem = Instantiate(prefabToSpawn, transform.position, transform.rotation);
-        
-        
+        // เชื่อม event pickup
         ItemPickup pickup = currentItem.GetComponent<ItemPickup>();
         if (pickup != null)
         {
@@ -48,5 +60,12 @@ public class ItemRespawnPoint : Item
     {
         yield return new WaitForSeconds(respawnTime);
         SpawnItem();
+    }
+
+    // แสดงกรอบใน Scene View
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(transform.position, areaSize);
     }
 }
